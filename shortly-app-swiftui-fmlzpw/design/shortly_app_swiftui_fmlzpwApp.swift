fe56f623fad49a7be32bb86161ed41a3bcf75340
Sungwook Kim <discerningacumen@gmail.com>
2021-09-03 00:34:20 +0900

    created @StateObject on shortly_app_swiftui_fmlzpwApp

--- shortly-app-swiftui-fmlzpw/design/shortly_app_swiftui_fmlzpwApp.swift
+++ shortly-app-swiftui-fmlzpw/design/shortly_app_swiftui_fmlzpwApp.swift
@@ -10,8 +10,13 @@ import SwiftUI
 @main
 struct shortly_app_swiftui_fmlzpwApp: App {
   
+  /// instantiating an empty data structure when the app is opened first and then it will persist.
+  @StateObject var dataStore = DataStore(urlpairs: [UrlAndShortened_Pair]())
+  
     var body: some Scene {
+      
         WindowGroup {
+          
             ContentView()
         }
     }
